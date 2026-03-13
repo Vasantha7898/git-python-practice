2976f9ecc6cc9898c6682d337ecdd6469a423762
Vasantha7898 <vasanthadurga5@gmail.com>
2026-03-13 06:39:19 +0530

    subtraction function added

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 def add(a,b):
     return a+b
 
+def subtract(a,b):
+    return a-b
 
-print("addition of two numbers",add(5+3))
+
+print("addition of two numbers",add(5,3))
+
+print("Subtarction of two numbers", subtract(5,2))
 
 
